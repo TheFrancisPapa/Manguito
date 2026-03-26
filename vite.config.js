4f72ab33c52d045bd6fc0e5796f811aec775572f
TheFrancisPapa <thefrancispapa@example.com>
2026-03-26 00:48:46 -0300

    fix(build): add tailwindvite plugin and explicit dist output for vercel

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate', // Se actualiza sola cuando subís cambios
       includeAssets: ['favicon.png', 'Mango.png'], // Los íconos que ya tenés en public/
